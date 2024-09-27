dad79abc8e74496ecfc24964db0ed1e24df39d72
ldrakohh <ldrakohh@gmail.com>
2024-09-27 18:12:19 +0200

    feat: actualizado index

--- frontend/src/home/index.js
+++ frontend/src/home/index.js
@@ -8,7 +8,7 @@ export default function Home(){
             <div className="hero-div">
                 <h1>Temis</h1>
                 <h3>---</h3>
-                <h3>Find the best vet for your pet</h3>                
+                <h3>Developed by Drako and GOMILA7</h3>                
             </div>
         </div>
     );
